9f7674a3c1d3ffdbc123064beb63e13b1990b682
VijaySinghSaaud <saudvijaysingh@gmail.com>
2023-11-16 10:59:15 +0530

    Kakn's algo

---
+++ Graph/topological_Kahns_Algo.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+void topologicalSort(int V, vector<int> adj[]) {
+
+    // Step 1: Find in-degrees
+    vector<int> inDegree(V, 0);
+    queue<int> q;
+
+    for (int u = 0; u < V; u++) {
+        for (int v : adj[u]) {
+            inDegree[v]++;
+        }
+    }
+
+    // Step 2: Enqueue all vertices with in-degree 0
+    for (int i = 0; i < V; i++) {
+        if (inDegree[i] == 0) {
+            q.push(i);
+        }
+    }
+
+    // Step 3: Process the queue (BFS)
+    vector<int> result;
+
+    while (!q.empty()) {
+        int frontNode = q.front();
+        q.pop();
+
+        // Add frontNode to the result
+        result.push_back(frontNode);
+
+        // Update in-degrees of neighbors
+        for (int neighbor : adj[frontNode]) {
+            inDegree[neighbor]--;
+            if (inDegree[neighbor] == 0) {
+                q.push(neighbor);
+            }
+        }
+    }
+
+    // Check for cycles (if not all vertices are included in the topological order)
+    if (result.size() != V) {
+        cout << "Graph contains a cycle, topological sort not possible." << endl;
+        return ;  // Return an empty vector in case of a cycle
+    }
+
+    // Print the topological order
+    cout << "Topological Order:";
+    for (int u : result) {
+        cout << " " << u;
+    }
+    cout << endl;
+
+    // return result;
+}
+
+int main() {
+    int V, E;
+    cout << "Enter the number of vertices and edges: ";
+    cin >> V >> E;
+
+    vector<int> adj[V];
+
+    cout << "Enter the edges (format: source destination):" << endl;
+    for (int i = 0; i < E; i++) {
+        int src, dest;
+        cin >> src >> dest;
+        adj[src].push_back(dest);
+    }
+
+    topologicalSort(V, adj);
+
+    return 0;
+}
